1e2e47fd578e5e9cd7bdbf25e391b9fa200f4f25
Ligas-X <41680200+Ligas-X@users.noreply.github.com>
2022-07-04 01:06:14 +0500

    Create Navbar element.

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Navbar } from "./src/Navbar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, world!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Navbar />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
